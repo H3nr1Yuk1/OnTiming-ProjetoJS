9dfa13ddda59f30fa489ddb7ebb9b00968be8100
Matheus <mathsilvaesilva@gmail.com>
2023-05-10 11:58:03 -0300

    Alteração de comentários

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,11 @@
 import express from 'express';
 import { Router } from './Config/routes';
 
-
 const app = express();
 
 app.use(express.json());
 app.use(Router);
 
-//O comando listen roda a aplicação
 app.listen(3000, function(){
     console.clear();
     console.log("Aplicação rodando na porta 3000");
