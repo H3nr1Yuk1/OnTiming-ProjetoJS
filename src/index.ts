cdb9d1c2f9e13c3245bf73a33bed8d972a58c23d
RafasPorto <rafaelkporto@gmail.com>
2023-05-10 11:43:18 -0300

    alteração nos comentarios

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-//Instalar express: npm install express
-//Instalar a tipagem do Express no projeto: npm i @types/express -D
-//Instalar TypeScript: npm install typescript -D
-//Inicializando o TypeScript: npx tsc --init
-//Instalar Tsx : npm i tsx -D
+
 //Chamando tudo que foi instalado: npm i
 //Instalar a extenção no RestClient no VSCode
 
