7cb4184901267c1210bb167f917bd529a7010e92
Matheus <mathsilvaesilva@gmail.com>
2023-05-10 11:29:34 -0300

    Criação da aplicação

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,18 @@
 //Instalar Tsx : npm i tsx -D
 //Chamando tudo que foi instalado: npm i
 //Instalar a extenção no RestClient no VSCode
+
+import express from 'express';
+import { Router } from './Config/routes';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(Router);
+
+//O comando listen roda a aplicação
+app.listen(3000, function(){
+    console.clear();
+    console.log("Aplicação rodando na porta 3000");
+});
