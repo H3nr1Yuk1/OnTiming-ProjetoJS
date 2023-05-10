ed0e674553bdc2547fb671970c00669387b105f2
Matheus <mathsilvaesilva@gmail.com>
2023-05-10 10:18:24 -0300

    Criação do index

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+//Instalar express: npm install express
+//Instalar a tipagem do Express no projeto: npm i @types/express -D
+//Instalar TypeScript: npm install typescript -D
+//Inicializando o TypeScript: npx tsc --init
+//Instalar Tsx : npm i tsx -D
+//Chamando tudo que foi instalado: npm i
