6c305884d26bf43aeef50e1cce9b850bf1728a35
Ygor Espada <ygorespada14@gmail.com>
2023-05-11 11:13:12 -0300

    Alteração comentario

--- src/Controller/categoria.controller.ts
+++ src/Controller/categoria.controller.ts
@@ -1,1 +1,1 @@
-//Criação de categorias: nomes, cor e icones, cores dezem ser feitas em um painel de cores ou espectro, os icones devem ser em formato de emojis ou icone
+//Criação de categorias: nomes, cor e icones, cores dezem ser feitas em um painel de cores ou espectro, os icones devem ser em formato de emojis ou icone (nome e cor em String)
