fb20f203b3371f40e6eeb71fa3044f0d1ad01ddf
Matheus <mathsilvaesilva@gmail.com>
2023-05-12 00:59:22 -0300

    Alteração e criação das categorias

---
+++ src/Models/categoria.model.ts
@@ -0,0 +1,11 @@
+//Criação de categorias: nomes, cor e icones, cores dezem ser feitas em um painel de cores ou espectro, os icones devem ser em formato de emojis ou icone (nome e cor em String)
+
+export class Categoria{
+    nome! : String;
+    cor! : String;
+    icone! : String;
+
+    constructor () {
+
+    }
+}
