e7ce7de9b72e3a70a6974da77ba50c526bf3723d
yguboy <ygorespada14@gmail.com>
2023-05-12 10:41:19 -0300

    Definição de rotas

--- src/Config/routes.ts
+++ src/Config/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { TarefaController } from "../Controller/tarefa.controller";
 
 const router : Router = Router();
 
-export { Router }
+router.post("/tarefas/criar", new TarefaController().criar);
+router.get("/tarefas/procurar", new TarefaController().procurar);
+
+export { router }
