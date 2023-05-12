5bc3411ed13ec6611633b91e30095e8248b3ffec
Matheus <mathsilvaesilva@gmail.com>
2023-05-12 01:00:30 -0300

    Alteração da classe Tarefa

--- src/Models/tarefa.model.ts
+++ src/Models/tarefa.model.ts
@@ -1,1 +1,16 @@
 //Criação de tarefa: tipo(Bloco de horas ou Rotina) ; Bloco de horas tem Nome, Duração, Categoria e Lembrete. ; Rotina contém tarefas de blocos de hora, nome e duração.
+
+import { Categoria } from "./categoria.model";
+import { Lembrete } from "./lembrete.models";
+
+export class Tarefa{
+    nome! : String;
+    tempoIni! : number;
+    tempoFim! : number;
+    categoria! : Categoria;
+    lembrete! : Lembrete;
+
+    constructor () {
+
+    }
+}
