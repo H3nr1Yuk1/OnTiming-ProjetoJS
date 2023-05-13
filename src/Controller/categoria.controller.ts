f3c03ae409f483bc9b25e99aa8e533a56b57b8a8
Matheus <mathsilvaesilva@gmail.com>
2023-05-13 02:45:34 -0300

    Metodos das categorias

--- src/Controller/categoria.controller.ts
+++ src/Controller/categoria.controller.ts
@@ -1,2 +1,26 @@
 import { Request , Response } from "express";
 import { Categoria } from "../Models/categoria.model";
+
+let listaCategoria : Categoria[] = [];
+
+export class CategoriaContoller {
+    criar(request : Request, response : Response) : Response {
+        return response;
+    }
+
+    remover(request : Request, response : Response) : Response {
+        return response;
+    }
+
+    atualizar(request : Request, response : Response) : Response {
+        return response;
+    }
+
+    listar(request : Request, response : Response) : Response {
+        return response;
+    }
+
+    procurar(request : Request, response : Response) : Response {
+        return response;
+    }
+}
