258dd7c0ce01841442efe1b405428748b4431741
Matheus <mathsilvaesilva@gmail.com>
2023-05-13 02:06:52 -0300

    Rotas de URL

--- src/Config/routes.ts
+++ src/Config/routes.ts
@@ -4,6 +4,9 @@ import { TarefaController } from "../Controller/tarefa.controller";
 const router : Router = Router();
 
 router.post("/tarefas/criar", new TarefaController().criar);
-router.get("/tarefas/procurar", new TarefaController().procurar);
+router.get("/tarefas/remover/:id", new TarefaController().remover);
+router.get("/tarefas/atualizar/:id", new TarefaController().atualizar);
+router.get("/tarefas/procurar/:id", new TarefaController().procurar);
+router.get("/tarefas/listar", new TarefaController().listar);
 
 export { router }
