880b42b58d8e93781e0f080ae6bb21aad61f4586
Matheus <mathsilvaesilva@gmail.com>
2023-05-13 11:49:58 -0300

    Atualização de rotas

--- src/Config/routes.ts
+++ src/Config/routes.ts
@@ -6,13 +6,13 @@ const router : Router = Router();
 
 router.post("/tarefas/criar", new TarefaController().criar);
 router.get("/tarefas/remover/:id", new TarefaController().remover);
-router.get("/tarefas/atualizar/:id", new TarefaController().atualizar);
+router.get("/tarefas/atualizar/:id", new TarefaController().atualizarTempo);
 router.get("/tarefas/procurar/:id", new TarefaController().procurar);
 router.get("/tarefas/listar", new TarefaController().listar);
 
 router.post("/categorias/criar", new CategoriaContoller().criar);
 router.get("/categorias/remover/:id", new CategoriaContoller().remover);
-router.get("/categorias/atualizar/:id", new CategoriaContoller().atualizar);
+router.get("/categorias/atualizar/:id", new CategoriaContoller().atualizarCor);
 router.get("/categorias/procurar/:id", new CategoriaContoller().procurar);
 router.get("/categorias/listar", new CategoriaContoller().listar)
 
