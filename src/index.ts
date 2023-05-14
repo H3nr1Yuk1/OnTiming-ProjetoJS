28acd59bb80046e6985e5c21384489b07d301079
Matheus <mathsilvaesilva@gmail.com>
2023-05-14 01:06:10 -0300

    Comentários de instalação

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 //Chamando tudo que foi instalado: npm i
 //Instalar a extenção no RestClient no VSCode
 
+//Instalação do prisma: npm install -g prisma
+//Prisma com Typescript: npm install @prisma/client prisma typescript
+
 import express from 'express';
 import { router } from './Config/routes';
 
