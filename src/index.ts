c8cf11a62169001425535bfc55de354ddcf1ac25
Matheus <mathsilvaesilva@gmail.com>
2023-05-14 22:23:40 -0300

    Banco de dados limpo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 //Instalar a extenção no RestClient no VSCode
 //Instalação do Prisma e o adaptando para TypeScript
 //Chamando tudo que foi instalado: npm i
-//Limpar banco de dados
-    //1 - delete o dev.db
-    //2 - npx prisma migrate dev
-    //3 - npx prisma migrate reset
-    //4 - npx prisma generate
+//Limpar banco de dados : npx prisma migrate reset
 //Ver banco de dados: https://inloop.github.io/sqlite-viewer/
 
 import express from 'express';
