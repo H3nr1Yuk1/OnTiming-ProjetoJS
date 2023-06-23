defa071a33615bcb76d19e665ed157bee87c42dc
Matheus <mathsilvaesilva@gmail.com>
2023-06-23 20:06:52 -0300

    Correções das rotinas

--- src/Controller/rotina.controller.ts
+++ src/Controller/rotina.controller.ts
@@ -1,94 +1,195 @@
 import { Request , Response } from "express";
 import { Rotina } from "../Models/rotina.model";
+import { PrismaClient } from "@prisma/client";
+import { Tarefa } from "../Models/tarefa.model";
 
-let listaRotinas : Rotina[] = [];
+const prisma = new PrismaClient();
 
 export class RotinaController {
-    criar(request : Request, response : Response) : Response {
-        const rotinaNova : Rotina = new Rotina();
-        rotinaNova.id = request.body.id;
 
-        for(let rotina of listaRotinas){
-            if(rotina.id == rotinaNova.id){
-                return response.status(404).json(
-                    {message : "Rotina já registrada!"}
-                );
+    async criar(request : Request, response : Response) {
+        let validar : boolean = false;
+        async function criarRotina(nome : string, duracao : string) {
+            let rotinaNova = null
+            try {
+                rotinaNova = await prisma.rotina.create({
+                    data : {
+                        nome : nome,
+                        duracao : duracao,
+                    }
+                })
+            } catch (error) {
+                return error;
+            } finally {
+                await prisma.$disconnect();
+                if(rotinaNova != null){
+                    validar = true;
+                    return rotinaNova;
+                }
             }
         }
 
-        rotinaNova.nome = request.body.nome;
-        rotinaNova.duracao = request.body.duracao;
-        rotinaNova.tarefas = request.body.tarefas;
+        const result = await criarRotina(request.body.nome, request.body.duracao)
 
-        listaRotinas.push(rotinaNova);
-
-        return response.status(201).json(
-            {message : "Rotina criada!", newRotina : rotinaNova}
-        );
-    }
-
-    remover(request : Request, response : Response) : Response {
-        const id = request.params.id;
-
-        for(let rotina of listaRotinas){
-            if(String(rotina.id) === id){
-                const index : number = listaRotinas.indexOf(rotina);
-                listaRotinas.splice(index, 1);
-
-                return response.status(200).json(
-                    {message : "Rotina removida"}
-                );
-            }
-        }
-
-        return response.status(404).json(
-            {message : "Rotina não encontrada!"}
-        );
-    }
-
-    atualizarTarefas(request : Request, response : Response) : Response {
-        const id = request.params.id;
-
-        for(let rotina of listaRotinas){
-            if(String(rotina.id) === id){
-                rotina.tarefas = request.body.tarefas;
-
-                return response.status(200).json(
-                    {message : "Rotina atualizada!", rotinaNova : rotina}
-                );
-            }
-        }
-
-        return response.status(404).json(
-            {message : "Rotina não encontrada!"}
-        );
-    }
-
-    procurar(request : Request, response : Response) : Response {
-        const id = request.params.id;
-
-        for(let rotina of listaRotinas){
-            if(String(rotina.id) === id){
-                return response.status(200).json(
-                    {message : "Rotina encontrada!", rotinaEnc : rotina}
-                );
-            }
-        }
-
-        return response.status(404).json(
-            {message : "Rotina não encontrada!"}
-        );
-    }
-
-    listar(request : Request, response : Response) : Response {
-        if (listaRotinas.length != 0){
-            return response.status(200).json(
-                {message : "Lista de rotinas", listagem : listaRotinas}
+        if(validar = true && result != null){
+            return response.status(201).json(
+                {message : "Rotina criada!", rotina : result}
             )
+        } else {
+            if (result === null){
+                return response.status(404).json(
+                    {message : "Rotina já existente!"}
+                )
+            } else {
+                return response.status(404).json(
+                    {message : "Erro!", erro : result}
+                )
+            }
+        }
+    }
+
+    async remover(request : Request, response : Response) {
+        let validar : boolean = false;
+        async function removerRotina(id : number) {
+            try {
+                await prisma.rotina.delete({
+                    where : {id : id}
+                });
+                validar = true;
+            } catch (error) {
+                return error;
+            } finally {
+                await prisma.$disconnect();
+            }
         }
 
-        return response.status(404).json(
-            {message : "Não há rotinas!"}
-        )
+        const result = await removerRotina(Number(request.params.id));
+        
+        if(validar = true){
+            return response.status(201).json(
+                {message : "Rotina removida!", rotina : result}
+            )
+        } else {
+            return response.status(404).json(
+                {message : "Rotina não existente!"}
+            )            
+        }
+    }
+
+    async atualizar(request : Request, response : Response) {
+        let validar : boolean = false;
+        async function atualizarRotina(id: number, nome: string, duracao: string) {
+            let rotinaAtualizada = null;
+            let validar = false;
+            try {
+              const rotinaAtualizada = await prisma.rotina.update({
+                where: { id : id },
+                data : {
+                    nome : nome,
+                    duracao : duracao,
+                }
+              });
+              validar = true;
+            } catch (error) {
+              return error;
+            } finally {
+              await prisma.$disconnect();
+              if (rotinaAtualizada != null) {
+                return rotinaAtualizada;
+              }
+            }
+          }
+
+        const result = await atualizarRotina(Number(request.params.id), request.body.nome, request.body.duracao)
+
+        if(validar = true && result != null){
+            return response.status(200).json(
+                {message : "Rotina atualizada!", rotina : result}
+            );
+        } else {
+            if (result === null) {
+                return response.status(404).json(
+                    {message : "Rotina não encontrada!"}
+                );
+            } else {
+                return response.status(404).json(
+                    {message : "Erro!", erro : result}
+                )
+            }
+        }
+    }
+
+    async procurar(request : Request, response : Response) {
+        let validar : boolean = false;
+        async function procurarRotina(id : number) {
+            let rotinaEncontrada = null;
+            try {
+                rotinaEncontrada = await prisma.rotina.findUnique({
+                    where: { id: id },
+                });
+            } catch (error) {
+                return error;
+            } finally {
+                await prisma.$disconnect();
+                if(rotinaEncontrada != null){
+                    validar = true;
+                    return rotinaEncontrada;
+                }
+            }
+        }
+
+        const result = await procurarRotina(Number(request.params.id))
+
+        if(validar = true && result != null){
+            return response.status(200).json(
+                {message : "Rotina encontrada!", rotina : result}
+            );
+        }else {
+            if(result === null){
+                return response.status(404).json(
+                    {message : "Rotina não encontrada!"}
+                );
+            } else {
+                return response.status(404).json(
+                    {message : "Erro!", erro : result}
+                )
+            }
+        }
+    }
+
+    async listar(request : Request, response : Response) {
+        let validar : boolean = true;
+        async function listarRotinas() {
+            let rotinas = null;
+            try {
+                rotinas = await prisma.rotina.findMany();
+            } catch (error) {
+                return error;
+            } finally {
+                await prisma.$disconnect();
+                if(rotinas != null){
+                    validar = true;
+                    return rotinas;
+                }
+            }
+        }
+
+        const result = await listarRotinas();
+
+        if(validar = true && result != null){
+            return response.status(200).json(
+                {message : "Lista de rotinas", rotinas : result}
+            )
+        } else {
+            if(result === null){
+                return response.status(404).json(
+                    {message : "Não há rotinas!"}
+                )
+            } else {
+                return response.status(404).json(
+                    {message : "Erro!", erro : result}
+                )
+            }
+        }        
     }
 }
