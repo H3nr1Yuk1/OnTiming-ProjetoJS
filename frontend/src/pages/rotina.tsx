5d8a359e3c71898bb0f2c78cae4058e910194b97
Matheus <mathsilvaesilva@gmail.com>
2023-06-25 19:59:13 -0300

    Alterações no frontend

--- frontend/src/pages/rotina.tsx
+++ frontend/src/pages/rotina.tsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from "react";
+import "./rotina.css"
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 function Rotina() { 
+
+    const [rotinas, setRotinas] = useState([]);
+
+    function listarRotinas(){
+        axios
+        .get("http://localhost:3001/rotinas/listar")
+        .then((resposta) => {
+            setRotinas(resposta.data.rotinas);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        listarRotinas();
+    })
+
     return (
         <>
         <div>
-
+            <div className="divBtn">
+                <button className="ButtonCriar">Criar Rotina</button>
+            </div>
+            <div className="listaRotinas">
+                <table>
+                    <tbody>
+                        {rotinas.map((rotina : any) => (
+                            <tr>
+                                <td className="nomeRotina">{rotina.nome}</td>
+                                <td className="duracaoRotina">{rotina.duracao}</td>
+                                <td className="lastTD">
+                                    <Link to={`/rotinas/acesso/${rotina.id}`}>Acessar</Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
         </>
     )
