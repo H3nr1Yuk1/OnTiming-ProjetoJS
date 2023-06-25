cf04fdaa6b29f736dd48b58a428373a76a20c591
RafasPorto <rafaelkporto@gmail.com>
2023-06-25 20:00:12 -0300

    alteração no front

--- frontend/src/pages/categoria.tsx
+++ frontend/src/pages/categoria.tsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Categoria() {
+
+    const [categoria, setCategoria] = useState([]);
+
+    function criarCategoria(){
+        axios
+        .get("http://localhost:3000/categorias/criar")
+        .then((resposta) => {
+            setCategoria(resposta.data.categoria)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <>
         <div>
-
+            <button onClick={criarCategoria}>Criar Categoria</button>
         </div>
         </>
     )
